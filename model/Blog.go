d6aa80c7dbd03ea1ffc63958535a4975d6e306f4
samuel <awesomeorji@gmail.com>
2019-02-15 13:07:18 +0300

    fixed formatting on Blog model

--- model/Blog.go
+++ model/Blog.go
@@ -1,12 +1,10 @@
 package model
 
 type Blog struct {
-
 	Id      int 	`json:"id"`
 	Title   string  `json:"title"`
 	Content string  `json:"content"`
 	Created string	`json:"created"`
 	Image   string  `json:"image"`
 	//author  Author
-
 }
